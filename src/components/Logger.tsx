567a8ae70026e64b5a8787fa9a95518a7b5b08cd
Mangiang <mangiang@orange.fr>
2020-07-21 20:35:41 -0400

    Fix the basic frontend

--- src/components/Logger.tsx
+++ src/components/Logger.tsx
@@ -1,27 +1,55 @@
 import React, { useEffect } from 'react'
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-import Stomp from 'stompjs'
 
-export function connectRabbit() {}
+// import { Client, Message, Frame } from '@stomp/stompjs'
+
+// export function connectRabbit() {}
 
 const Logger = () => {
-  
   useEffect(() => {
-    const ws = new WebSocket('ws:///localhost/rabbitmq/ws')
-    const stompClient = Stomp.over(ws)
-    const headers = {
-      login: 'user',
-      passcode: 'user',
-      durable: 'true',
-      'auto-delete': 'false',
-    }
+    const client = new W3CWebSocket('ws://localhost/ws');
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message: object) => {
+      console.log(message);
+    };
 
-    stompClient.connect(headers, function (frame) {
-      console.log('Connected')
-      const subscription = stompClient.subscribe('/queue/hello', function ( message,) {
-        console.log(message)
-      })
-    })
+    // const client = new Client({
+    //   brokerURL: 'ws:///localhost/rabbitmq/ws',
+    //   connectHeaders: {
+    //     login: 'user',
+    //     passcode: 'user',
+    //   },
+    //   debug: function (str: string) {
+    //     console.log(str)
+    //   },
+    //   reconnectDelay: 2000,
+    //   heartbeatIncoming: 4000,
+    //   heartbeatOutgoing: 4000,
+    // })
+
+    // client.onConnect = function (frame: Frame) {
+    //   // Do something, all subscribes must be done is this callback
+    //   // This is needed because this will be executed after a (re)connect
+    //   const subscription = client.subscribe('/queue/hello', function (
+    //     message: Object,
+    //   ) {
+    //     console.log(message)
+    //   })
+    // }
+
+    // client.onStompError = function (frame: Frame) {
+    //   // Will be invoked in case of error encountered at Broker
+    //   // Bad login/passcode typically will cause an error
+    //   // Complaint brokers will set `message` header with a brief message. Body may contain details.
+    //   // Compliant brokers will terminate the connection after any error
+    //   console.log('Broker reported error: ' + frame.headers['message'])
+    //   console.log('Additional details: ' + frame.body)
+    // }
+
+    // client.activate()
   }, [])
 
   const click = () => {
