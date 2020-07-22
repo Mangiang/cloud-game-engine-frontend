fb976d97fcf66115dafb978bda87dc92e0fdf14e
Mangiang <mangiang@orange.fr>
2020-07-21 23:53:00 -0400

    Add IframeResizer for basic plugin system

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
 import Logger from './components/Logger';
+import IframeResizer from 'iframe-resizer-react'
 
 const App = () => {
   return (
     <div className="App">
+      <IframeResizer
+        src="http://localhost/plugins/logger"
+        style={{ width: '1px', minWidth: '100%'}}
+      />
       <Logger />
     </div>
   );
